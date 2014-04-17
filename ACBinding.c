b39a2da0a4dc652dc00ae995bae582b1ba87235d
vollero <vollero@gmail.com>
2014-04-18 01:40:06 +0200

    Update ACBinding.c

--- ACBinding.c
+++ ACBinding.c
@@ -1,19 +1,20 @@
-/************************************************************************************************
- * Copyright (c) 2006-2009 Laboratorio di Sistemi di Elaborazione e Bioingegneria Informatica	*
- *                          Universita' Campus BioMedico - Italy								*
- *																								*
- * This program is free software; you can redistribute it and/or modify it under the terms		*
- * of the GNU General Public License as published by the Free Software Foundation; either		*
- * version 2 of the License, or (at your option) any later version.								*
- *																								*
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY				*
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A				*
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.						*
- *																								*
- * You should have received a copy of the GNU General Public License along with this			*
- * program; if not, write to the:																*
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,							*
- * MA  02111-1307, USA.																			*
+/*******************************************************************************************
+ * Copyright (c) 2006-2009 Laboratorio di Sistemi di Elaborazione e Bioingegneria 	   *
+ *                          Informatica	Universita' Campus BioMedico - Italy	           *
+ *											   *
+ * This program is free software; you can redistribute it and/or modify it under the terms *
+ * of the GNU General Public License as published by the Free Software Foundation; either  *
+ * version 2 of the License, or (at your option) any later version.			   *
+ *											   *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY	   *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A	   *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.		   *
+ *											   *
+ * You should have received a copy of the GNU General Public License along with this       *
+ * program; if not, write to the:							   *
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,			   *
+ * MA  02111-1307, USA.									   *
+ *                                                                                         *
  * In addition, as a special exception, the copyright holders give permission to link the  *
  * code of portions of this program with the OpenSSL library under certain conditions as   *
  * described in each individual source file, and distribute linked combinations including  * 
@@ -23,18 +24,18 @@
  * so.  If you do not wish to do so, delete this exception statement from your version.    *
  * If you delete this exception statement from all source files in the program, then also  *
  * delete it here.                                                                         *
- *																								*
- * -------------------------------------------------------------------------------------------- *
- * Project:  Capwap																				*
- *																								*
- * Authors : Ludovico Rossi (ludo@bluepixysw.com)												*  
- *           Del Moro Andrea (andrea_delmoro@libero.it)											*
- *           Giovannini Federica (giovannini.federica@gmail.com)								*
- *           Massimo Vellucci (m.vellucci@unicampus.it)											*
- *           Mauro Bisson (mauro.bis@gmail.com)													*
- *           Daniele De Sanctis (danieledesanctis@gmail.com)									* 
- *	         Antonio Davoli (antonio.davoli@gmail.com)											*
- ************************************************************************************************/
+ *											   *
+ * --------------------------------------------------------------------------------------- *
+ * Project:  Capwap									   *
+ *											   *
+ * Authors : Ludovico Rossi (ludo@bluepixysw.com)					   *  
+ *           Del Moro Andrea (andrea_delmoro@libero.it)					   *
+ *           Giovannini Federica (giovannini.federica@gmail.com)		           *
+ *           Massimo Vellucci (m.vellucci@unicampus.it)					   *
+ *           Mauro Bisson (mauro.bis@gmail.com)						   *
+ *           Daniele De Sanctis (danieledesanctis@gmail.com)				   * 
+ *	     Antonio Davoli (antonio.davoli@gmail.com)				           *
+ *******************************************************************************************/
 
 
 #include "CWAC.h"
