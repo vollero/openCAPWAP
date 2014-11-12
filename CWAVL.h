a8cde2b9834e0c416bd68d5f0d5d4f6bf6f31c89
root <elena.ago@gmail.com>
2014-11-12 12:31:26 +0100

    AVL test

---
+++ CWAVL.h
@@ -0,0 +1,30 @@
+typedef struct nodeAVL
+{
+	//WTP: index == BSS index
+	//AC: index == WTP index
+    int index;
+    unsigned char staAddr[ETH_ALEN];
+    struct nodeAVL*  left;
+    struct nodeAVL*  right;
+    int      height;
+} nodeAVL;
+
+extern nodeAVL * avlTree;
+extern CWThreadMutex mutexAvlTree;
+
+void AVLdispose(nodeAVL* t);
+nodeAVL* AVLfind( unsigned char * staAddr, nodeAVL *t );
+nodeAVL* AVLfind_min( nodeAVL *t );
+nodeAVL* AVLfind_max( nodeAVL *t );
+nodeAVL* AVLinsert( int index, unsigned char * staAddr, nodeAVL *t );
+struct nodeAVL* AVLdeleteNode(struct nodeAVL* root, unsigned char * staAddr);
+void AVLdisplay_avl(nodeAVL* t);
+int AVLget( nodeAVL* n );
+int AVLgetBalance(struct nodeAVL *N);
+
+nodeAVL* AVLsingle_rotate_with_left( nodeAVL* k2 );
+nodeAVL* AVLsingle_rotate_with_right( nodeAVL* k2 );
+nodeAVL* AVLdouble_rotate_with_left( nodeAVL* k3 );
+nodeAVL* AVLdouble_rotate_with_right( nodeAVL* k3 );
+
+int compareEthAddr(unsigned char * staAddr, unsigned char * AVLaddr);
